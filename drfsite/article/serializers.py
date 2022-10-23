19fe45749114e89b5a392f29672b0d96b94c3305
Egor <egor_kuplinov@mail.ru>
2022-10-23 18:22:40 +0400

    change Serializer to ModelSerializer

--- drfsite/article/serializers.py
+++ drfsite/article/serializers.py
@@ -3,18 +3,24 @@ from rest_framework import serializers
 from article.models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    content = serializers.CharField()
-    time_created = serializers.DateTimeField(read_only=True)
-    cat_id = serializers.IntegerField()
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = "__all__"
 
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.content = validated_data.get('content', instance.content)
-        instance.cat_id = validated_data.get('cat_id', instance.cat_id)
-        instance.save()
-        return instance
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=100)
+#     content = serializers.CharField()
+#     time_created = serializers.DateTimeField(read_only=True)
+#     cat_id = serializers.IntegerField()
+#
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.content = validated_data.get('content', instance.content)
+#         instance.cat_id = validated_data.get('cat_id', instance.cat_id)
+#         instance.save()
+#         return instance
